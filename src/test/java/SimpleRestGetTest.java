13e1d7ff101544520fe1a2bf527027ff00612e65
sachdd <deepika.sachdeva@airnz.co.nz>
2021-05-14 14:35:17 +1200

    Added a new test

--- src/test/java/SimpleRestGetTest.java
+++ src/test/java/SimpleRestGetTest.java
@@ -1,19 +1,36 @@
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
+import io.restassured.internal.path.json.mapping.JsonObjectDeserializer;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
 
 public class SimpleRestGetTest {
 
-   @Test
-    public void getWeatherDetails()
+    @Test
+    public void sampleTestOne()
    {
        RestAssured.baseURI = "http://pokeapi.co/api/v2/pokemon/ditto";
        RequestSpecification httpRequest = RestAssured.given();
        Response response = httpRequest.request(Method.GET);
        String responseBody = response.getBody().asString();
-       System.out.println("Response Body is=>>" + responseBody);
+   }
+
+   @Test
+    public void sampleTestTwo()
+   {
+       RestAssured.baseURI = "http://bookstore.toolsqa.com";
+       RequestSpecification httpRequest = RestAssured.given();
+       httpRequest.header("Content-Type","application/json");
+       Response response = httpRequest.get("/BookStore/v1/Books");
+       String jsonString = response.asString();
+       List<Map<String,String>> books = JsonPath.from(jsonString).get("books");
+       Assert.assertTrue(books.size() > 0);
    }
 
 }
